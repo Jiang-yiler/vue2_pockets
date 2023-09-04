328ab09ffb461a415558cff93b5e2ba57e08eed4
ChenH <chenhao6319@163.com>
2023-09-04 23:24:50 +0800

    refactor DynamicHight

--- src/components/LongList/VirtualList/DynamicHeight.vue
+++ src/components/LongList/VirtualList/DynamicHeight.vue
@@ -8,8 +8,8 @@
     <div ref="phantom" class="virtual-list-phantom"></div>
     <div ref="content" class="virtual-list">
       <div
-        class="virtual-list-item"
         ref="items"
+        class="virtual-list-item"
         :id="item.id"
         :key="item.id"
         v-for="item in visibleData"
@@ -42,24 +42,34 @@ export default {
       default: 0,
     },
   },
+  data() {
+    return {
+      positions: [],
+      startIndex: 0,
+      onScroll: null,
+    };
+  },
   computed: {
     // 这个影响的其实是渲染的条目数，保证比可以看到的大就可以了
     visibleCount() {
       return Math.ceil(this.visibleHeight / this.estimatedItemHeight);
     },
-    aboveCount() {
-      return Math.min(this.start, this.bufferScale * this.visibleCount);
+    endIndex() {
+      return this.startIndex + this.visibleCount;
     },
-    belowCount() {
+    topBufferSize() {
+      return Math.min(this.startIndex, this.bufferScale * this.visibleCount);
+    },
+    bottomBufferSize() {
       return Math.min(
-        this.listData.length - this.end,
+        this.listData.length - this.endIndex,
         this.bufferScale * this.visibleCount
       );
     },
     visibleData() {
-      let start = this.start - this.aboveCount;
-      let end = this.end + this.belowCount;
-      return this.listData.slice(start, end);
+      let renderStart = this.startIndex - this.topBufferSize;
+      let renderEnd = this.endIndex + this.bottomBufferSize;
+      return this.listData.slice(renderStart, renderEnd);
     },
   },
   created() {
@@ -67,26 +77,17 @@ export default {
     this.onScroll = throttle(this.handleScroll, 200, { leading: false });
   },
   mounted() {
-    this.start = 0;
-    this.end = this.start + this.visibleCount;
+    this.updateStartIndex();
   },
   updated() {
     this.$nextTick(function () {
       if (!this.$refs.items || !this.$refs.items.length) {
         return;
       }
-      this.updateItemsSize();
+      this.updatePositions();
       let height = this.positions[this.positions.length - 1].bottom;
       this.$refs.phantom.style.height = height + "px";
-      this.getStartOffset();
     });
-  },
-  data() {
-    return {
-      positions: [],
-      start: 0,
-      end: 0,
-    };
   },
   methods: {
     initPositions() {
@@ -97,8 +98,44 @@ export default {
         bottom: (index + 1) * this.estimatedItemHeight,
       }));
     },
-    getStartIndex(scrollTop = 0) {
-      return this.binarySearch(this.positions, scrollTop);
+    // 在updated钩子中获取真实的位置信息
+    updatePositions() {
+      let nodes = this.$refs.items;
+      nodes.forEach((node) => {
+        let rect = node.getBoundingClientRect();
+        let height = rect.height;
+        let index = +node.id.slice(1);
+        let oldHeight = this.positions[index].height;
+        let dValue = oldHeight - height;
+        if (dValue) {
+          this.positions[index].bottom = this.positions[index].bottom - dValue;
+          this.positions[index].height = height;
+          for (let k = index + 1; k < this.positions.length; k++) {
+            this.positions[k].top = this.positions[k - 1].bottom;
+            this.positions[k].bottom = this.positions[k].bottom - dValue;
+          }
+        }
+      });
+    },
+    handleScroll() {
+      let scrollTop = this.$refs.list.scrollTop;
+      this.updateStartIndex(scrollTop);
+      this.updateOffset();
+    },
+    updateStartIndex(scrollTop = 0) {
+      // 1. 基本计算
+      let start = this.positions.find(
+        (item) => item && item.bottom >= scrollTop
+      ).index;
+      // 2. 使用二分查找优化
+      // let start = this.binarySearch(this.positions, scrollTop);
+      this.startIndex = start;
+    },
+    updateOffset() {
+      const offset = this.positions[this.startIndex - this.topBufferSize]
+        ? this.positions[this.startIndex - this.topBufferSize].top
+        : 0;
+      this.$refs.content.style.transform = `translate3d(0,${offset}px,0)`;
     },
     binarySearch(list, value) {
       let start = 0;
@@ -120,45 +157,6 @@ export default {
         }
       }
       return tempIndex;
-    },
-    updateItemsSize() {
-      let nodes = this.$refs.items;
-      nodes.forEach((node) => {
-        let rect = node.getBoundingClientRect();
-        let height = rect.height;
-        let index = +node.id.slice(1);
-        let oldHeight = this.positions[index].height;
-        let dValue = oldHeight - height;
-        if (dValue) {
-          this.positions[index].bottom = this.positions[index].bottom - dValue;
-          this.positions[index].height = height;
-          for (let k = index + 1; k < this.positions.length; k++) {
-            this.positions[k].top = this.positions[k - 1].bottom;
-            this.positions[k].bottom = this.positions[k].bottom - dValue;
-          }
-        }
-      });
-    },
-    getStartOffset() {
-      let startOffset;
-      if (this.start >= 1) {
-        let size =
-          this.positions[this.start].top -
-          (this.positions[this.start - this.aboveCount]
-            ? this.positions[this.start - this.aboveCount].top
-            : 0);
-        startOffset = this.positions[this.start - 1].bottom - size;
-      } else {
-        startOffset = 0;
-      }
-      this.$refs.content.style.transform = `translate3d(0,${startOffset}px,0)`;
-    },
-    handleScroll() {
-      let scrollTop = this.$refs.list.scrollTop;
-      // this.start = this.getStartIndex(scrollTop);
-      this.start = this.positions.find((i) => i && i.bottom > scrollTop).index;
-      this.end = this.start + this.visibleCount;
-      this.getStartOffset();
     },
   },
 };
